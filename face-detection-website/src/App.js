fef75afe37b81bc60a5268f7233637303441a3dd
Sajad <sajadalipour@aut.ac.ir>
2020-08-22 23:44:54 +0430

    navigation

--- face-detection-website/src/App.js
+++ face-detection-website/src/App.js
@@ -1,11 +1,16 @@
 import React,{Component} from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 import './App.css';
 
 class App extends Component{
   render(){
     return (
       <div className="App">
-        
+        <Navigation />
+        <Logo />
+        {/* <ImageLinkForm />
+        <FaceRecognition /> */} 
       </div>
     );
   }
