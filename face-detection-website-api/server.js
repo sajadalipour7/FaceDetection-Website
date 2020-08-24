3240294f5197a27a3737dc9b50d188cf08fc04c1
Sajad <sajadalipour@aut.ac.ir>
2020-08-24 17:49:02 +0430

    Back-end Signin

---
+++ face-detection-website-api/server.js
@@ -0,0 +1,51 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const app=express();
+
+app.use(bodyParser.json());
+
+const database={
+	users:[
+		{
+			id:'123',
+			name:'John',
+			email:'john@gmail.com',
+			password:'cookies',
+			entries:0,
+			joined:new Date()
+		},
+		{
+			id:'124',
+			name:'Sally',
+			email:'sally@gmail.com',
+			password:'bananas',
+			entries:0,
+			joined:new Date()
+		}
+	]
+}
+app.get('/',(req,res)=>{
+	res.send('this is working');
+})
+
+app.post('/signin',(req,res)=>{
+	if(req.body.email===database.users[0].email &&
+		req.body.password===database.users[0].password){
+			res.json('Success');
+		}else{
+			res.status(400).json('Error logging in!');
+		}
+})
+app.listen(3000,()=>{
+	console.log('app is running on port 3000');
+})
+
+
+/*
+/ --> res=this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+
+*/
