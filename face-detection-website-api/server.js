9af081c0dfa9fdec754018832df05e3cc73746f6
Sajad <sajadalipour@aut.ac.ir>
2020-08-31 15:55:12 +0430

    Getting User Profile from DataBase

--- face-detection-website-api/server.js
+++ face-detection-website-api/server.js
@@ -98,16 +98,23 @@ app.post('/register',(req,res)=>{
 
 app.get('/profile/:id',(req,res)=>{
 	const {id}=req.params;
-	let found=false;
-	database.users.forEach(user=>{
-		if(user.id===id){
-			found=true;
-			return res.json(user);
+	// let found=false;
+	db.select('*').from('users')
+	.where(
+		//Or we can say:
+		//{id:id}
+		{id}
+	)
+	.then(user=>{
+		// console.log(user[0]);
+		if(user.length){
+			res.json(user[0]);
+		}else{
+			res.status(400).json('Not found');
 		}
 	})
-	if(!found){
-		res.status(400).json('not found');
-	}
+	.catch(err=>res.status(400).json('error getting user'));
+	
 })
 app.put('/image',(req,res)=>{
 	const {id}=req.body;
