c43af9f13af7378b3bc8b7e9bef2d30df8053bdb
Sajad <sajadalipour@aut.ac.ir>
2020-09-01 20:23:39 +0430

    debug

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image=require('./controllers/image');
 const db=knex({
 	client:'pg',
 	connection:{
-		connectionString:process.env.DATABASE_URL,
+		connectionnString:process.env.DATABASE_URL,
 		ssl:true,
 	}
 })
