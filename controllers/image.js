bbbd24057acd223c52a2b7bcf90fae491a4d6703
Sajad <sajadalipour@aut.ac.ir>
2020-09-01 19:35:35 +0430

    Heroku

---
+++ controllers/image.js
@@ -0,0 +1,42 @@
+const Clarifai =require('clarifai');
+
+const app=new Clarifai.App({
+	apiKey:'b56c9c7b369a43d9a04b395cee551278'
+});
+const handleApiCall=(req,res)=>{
+
+	app.models
+	.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+	.then(data=>{
+		res.json(data);
+	})
+	.catch(err=>res.status(400).json("unable to work with api"));
+}
+const handleImage=(req,res,db)=>{
+	const {id}=req.body;
+	db('users').where('id','=',id)
+	.increment('entries',1)
+	.returning('entries')
+	.then(entries=>{
+		res.json(entries[0]);
+	})
+	.catch(err=>{
+		res.status(400).json("unable to get entries");
+	});
+	// let found=false;
+	// database.users.forEach(user=>{
+	// 	if(user.id===id){
+	// 		found=true;
+	// 		user.entries++;
+	// 		return res.json(user.entries);
+	// 	}
+	// })
+	// if(!found){
+	// 	res.status(400).json('not found');
+	// }
+}
+
+module.exports={
+	handleImage:handleImage,
+	handleApiCall:handleApiCall
+}
