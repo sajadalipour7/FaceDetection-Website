92455cd524f9b967f5c5dc3506d886a1bf0d3a37
Sajad <sajadalipour@aut.ac.ir>
2020-09-01 20:09:10 +0430

    db connection

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const image=require('./controllers/image');
 const db=knex({
 	client:'pg',
 	connection:{
-		host:'postgresql-flat-75706',
-		user:'postgres',
-		password:'',
-		database:'smart_brain'
+		host:process.env.DATABASE_URL,
+		ssl:true
 	}
 })
 
