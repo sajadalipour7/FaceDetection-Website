4d15680d16bad9237467bb598f97c48cd07621af
Sajad <sajadalipour@aut.ac.ir>
2020-09-01 20:37:46 +0430

    dg debug

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const profile=require('./controllers/profile');
 const image=require('./controllers/image');
 const db=knex({
 	client:'pg',
-	connection:{
-		connectionString:process.env.DATABASE_URL,
-		ssl:true
-	}
+	connectionString: process.env.DATABASE_URL,
+    ssl: {
+    	rejectUnauthorized: false
+  	}
 });
 
 // db.select('*').from('users')
